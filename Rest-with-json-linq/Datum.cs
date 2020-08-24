a46839b153589219486f17322e7bc507f1682eb5
Kaosilisochukwu Nwizu <kcharlse94@gmail.com>
2020-08-24 23:02:41 +0100

    Add classes for Json deserialization

---
+++ Rest-with-json-linq/Datum.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.Json.Serialization;
+
+namespace Rest_with_json_linq
+{
+    public class Datum
+    {
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public string About { get; set; }
+        public int Submitted { get; set; }
+
+        [JsonPropertyName("updated_at")]
+        public DateTime UpdatedAt { get; set; }
+
+        [JsonPropertyName("submission_count")]
+        public int SubmissionCount { get; set; }
+
+        [JsonPropertyName("comment_count")]
+        public int CommentCount { get; set; }
+
+        [JsonPropertyName("created_at")]
+        public int CreatedAt { get; set; }
+    }
+}
---
+++ Rest-with-json-linq/Root.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.Json.Serialization;
+
+namespace Rest_with_json_linq
+{
+    public class Root
+    {
+        public string Page { get; set; }
+
+        [JsonPropertyName("per_page")]
+        public int PerPage { get; set; }
+        public int Total { get; set; }
+
+        [JsonPropertyName("total_pages")]
+        public int TotalPages { get; set; }
+        public List<Datum> Data { get; set; }
+    }
+}
